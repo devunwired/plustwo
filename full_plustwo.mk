cb84d0425a7030c6b76be159346e05abfc6f33d2
Dave Smith <smith@wiresareobsolete.com>
2014-05-14 22:30:03 -0600

    Lockscreen property.

--- full_plustwo.mk
+++ full_plustwo.mk
@@ -12,3 +12,7 @@ DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 # Copy custom boot animation
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+# Default disable the lock screen
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.lockscreen.disable.default=true
