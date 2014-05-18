639ab66f318a8f2b451658c3b6cffd28a2afa36e
Dave Smith <smith@wiresareobsolete.com>
2014-05-17 22:17:18 -0600

    Initial add of service project.

---
+++ app/SysInfoService/src/com/doubleencore/sysinfoservice/TestActivity.java
@@ -0,0 +1,15 @@
+package com.doubleencore.sysinfoservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
